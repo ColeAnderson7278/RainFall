202d8505435e1aae7fdac690c3e9c214e0626dad
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-10-23 15:18:43 -0500

    Roughly Got Character To Move
    I've used Nate and my code to start to work on a functioning
    game. I just got the character to move with the keys, but alot
    of other issues need to be addressed.

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ class RainFall {
 
     initializePlayer() {
         this.player = {
-            x: 50
+            x: 325
         };
     }
 
@@ -26,11 +26,11 @@ class RainFall {
     }
 
     moveLeft() {
-        this.player.x -= 5;
+        this.player.x -= 20;
     }
 
     moveRight() {
-        this.player.x += 5;
+        this.player.x += 20;
     }
 
     isPlayerHit() {
@@ -55,31 +55,72 @@ class RainFall {
     }
 }
 
-class DOMRainFall {
-    constructor(rootSelector) {
-        this.gameState = new RainFall();
-        this.root = document.querySelector(rootSelector);
-        this.setupDOM();
-    }
+var newGame = new RainFall();
 
-    setupDOM() {
-        this.setupPlayer();
-        this.setupRainDrops();
-    }
-
-    setupPlayer() {
-        this.playerElement = document.createElement("span");
-        this.playerElement.classList.add("rainfall__player");
-        this.root.appendChild(playerElement);
-        this.playerElement.updatePlayerLocation();
-    }
-
-    updatePlayerLocation() {
-        this.playerElement.style.left = `${this.player.x}%`;
-    }
-
-    tick() {
-        this.gameState.tick();
-        this.updateDOM();
-    }
+function updatePlayerLocation() {
+    var user = document.querySelector(".user");
+    user.style.left = `${newGame.player.x}px`;
 }
+
+document.addEventListener("keypress", function(event) {
+    if (event.key === "ArrowLeft") {
+        newGame.moveLeft();
+    } else if (event.key === "ArrowRight") {
+        newGame.moveRight();
+    }
+});
+
+function tick() {
+    updatePlayerLocation();
+}
+
+setInterval(tick, 25);
+
+// class DOMRainFall {
+//     constructor(rootSelector) {
+//         this.gameState = new RainFall();
+//         this.root = document.querySelector(rootSelector);
+//         this.setupDOM();
+//     }
+
+//     setupDOM() {
+//         this.setupPlayer();
+//         this.setupRainDrops();
+//     }
+
+//     setupPlayer() {
+//         this.playerElement = document.querySelector(".user");
+//     }
+
+//     setupRainDrops() {
+//         for (var rainDrop of this.gameState.rainDrops) {
+//             var newDrop = `<div class="rain" style="top: ${
+//                 rainDrop.y
+//             }px; left: ${rainDrop.x}px;"></div>`;
+//             this.root
+//                 .querySelector(".rainFall")
+//                 .insertAdjacentElement("beforebegin", newDrop);
+//         }
+//     }
+
+//     updatePlayerLocation() {
+//         this.playerElement.style.left = `${this.player.x}%`;
+//     }
+
+//     tick() {
+//         this.gameState.tick();
+//         this.updateDOM();
+//     }
+// }
+
+// var newGame = new DOMRainFall(".gameContainer");
+
+// document.addEventListener("keypress", function(event) {
+//     if (event.key === "ArrowRight") {
+//         newGame.gameState.moveRight();
+//     } else if (event.key === "ArrowLeft") {
+//         newGame.gameState.moveLeft();
+//     }
+// });
+
+// setInterval(newGame.tick, 500);
