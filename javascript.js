78e8f461b2b18095a71e19b8117571fef5d557ed
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-10-23 14:04:04 -0500

    Worked on JS with Nate's Help
    Nate helped me establish a solid backbone to my code. We
    did this by adding in two classes. One for the background
    information, and one that took that info and used it to
    display the game to the user.

--- javascript.js
+++ javascript.js
@@ -1,27 +1,85 @@
-function moveCharacterRight() {
-    var user = document.querySelector(".user");
-    var positionNum = document.querySelector(".positionNum");
-    if (positionNum.innerText < 450) {
-        positionNum.innerText = Number(positionNum.innerText) + 10;
-        user.style.remove = "left";
-        user.style.left = `${positionNum.innerText}px`;
+class RainFall {
+    constructor() {
+        this.initializePlayer();
+        this.generateRainDrops();
+        this.gameover = false;
+    }
+
+    initializePlayer() {
+        this.player = {
+            x: 50
+        };
+    }
+
+    generateRainDrops() {
+        this.rainDrops = [];
+        for (let i = 0; i < 10; i++) {
+            this.generateRainDrop();
+        }
+    }
+
+    generateRainDrop() {
+        this.rainDrops.push({
+            x: Math.floor(Math.random() * 100),
+            y: 100
+        });
+    }
+
+    moveLeft() {
+        this.player.x -= 5;
+    }
+
+    moveRight() {
+        this.player.x += 5;
+    }
+
+    isPlayerHit() {
+        return this.rainDrops.some(d => d.x === this.player.x && d.y === 0);
+    }
+
+    dropRainDrops() {
+        for (const rainDrop in this.rainDrops) {
+            rainDrop.x -= 5;
+        }
+    }
+
+    checkForEndGame() {
+        this.gameover = this.isPlayerHit();
+    }
+
+    tick() {
+        this.dropRainDrops();
+        this.removeFallenRainDrops();
+        this.addMoreRainDrops();
+        this.checkForEndGame();
     }
 }
 
-function moveCharacterLeft() {
-    var user = document.querySelector(".user");
-    var positionNum = document.querySelector(".positionNum");
-    if (positionNum.innerText > -1) {
-        positionNum.innerText = Number(positionNum.innerText) - 10;
-        user.style.remove = "left";
-        user.style.left = `${positionNum.innerText}px`;
+class DOMRainFall {
+    constructor(rootSelector) {
+        this.gameState = new RainFall();
+        this.root = document.querySelector(rootSelector);
+        this.setupDOM();
+    }
+
+    setupDOM() {
+        this.setupPlayer();
+        this.setupRainDrops();
+    }
+
+    setupPlayer() {
+        this.playerElement = document.createElement("span");
+        this.playerElement.classList.add("rainfall__player");
+        this.root.appendChild(playerElement);
+        this.playerElement.updatePlayerLocation();
+    }
+
+    updatePlayerLocation() {
+        this.playerElement.style.left = `${this.player.x}%`;
+    }
+
+    tick() {
+        this.gameState.tick();
+        this.updateDOM();
     }
 }
-
-document.addEventListener("keypress", function(event) {
-    if (event.key === "ArrowRight") {
-        moveCharacterRight();
-    } else if (event.key === "ArrowLeft") {
-        moveCharacterLeft();
-    }
-});
